2a6f49b6c8a4755d4ec9e1b663a7770ec174df65
Lasthunter657 <ali.redha.ali@gmail.com>
2022-08-23 16:19:31 +0300

    done

--- practice/nice.m
+++ practice/nice.m
@@ -5,9 +5,15 @@
 
 % fplot(triangularPulse(x), [-2 2])
 
-syms t s
-% f=t*exp(-4*t); 
-% laplace(f)
+syms z 
 
-f= s*(s+6)/((s+3)*(s^2+6*s+18))
-ilaplace(f)
+f1 =  ((z-1)*(z+0.8))/((z-0.5)*(z+0.2));
+f2 = ((z+0.8))/((z+0.2)*(z-0.5));
+f3 = (z^3+z+1)/((z^2+0.5*z+0.25)*(z-1));
+f4 = ((z^2-1)*(z+0.8))/((z-0.5)^2*(z+0.2));
+
+
+iztrans(f1) 
+iztrans(f2)
+iztrans(f3)
+iztrans(f4)
